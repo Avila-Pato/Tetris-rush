5fe8694742a6bf30346256ac14afb0006af5f319
Avila-Pato <152195893+Avila-Pato@users.noreply.github.com>
2024-05-01 21:18:47 -0400

    Yio :<

---
+++ public/consts.js
@@ -0,0 +1,10 @@
+// Refactorizando
+export const BLOCK_SIZE = 20
+export const BOARD__WIDTH = 14
+export const BOARD__HEIGHT = 30
+
+export const EVENT_MOVEMENTS = {
+  LEFT: 'ArrowLeft',
+  DOWN: 'ArrowDown',
+  RIGHT: 'ArrowRight'
+}
